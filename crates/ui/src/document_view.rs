148a503e98d19073a64d9d0f6f8da32208384318
Romain TRUCHI <romain.truchi.06@gmail.com>
2023-06-14 22:25:41 +0200

    Back to rendering selections
    WIP

--- crates/ui/src/document_view.rs
+++ crates/ui/src/document_view.rs
@@ -332,6 +332,7 @@ impl<'a, 'b, 'c, 'd, 'e> Renderer<'a, 'b, 'c, 'd, 'e> {
         self.highlights();
         self.render_line_numbers();
         self.render_document((self.line_numbers_width + self.advance).ceil() as i32);
+        self.render_selection();
     }
 
     fn highlights(&mut self) {
@@ -437,5 +438,153 @@ impl<'a, 'b, 'c, 'd, 'e> Renderer<'a, 'b, 'c, 'd, 'e> {
                 self.view.line_height as u32,
             );
         }
+    }
+
+    fn render_selection(&mut self) {
+        const COLOR: Rgba = Rgba::WHITE;
+
+        let selection = self.document.selection();
+        let draw_width = self.draw.width();
+
+        let line_top = |line| line as i32 * self.view.line_height as i32 - self.scroll_top as i32;
+
+        // Not sure what is best when glyph ranges and cursor columns don't align...!
+        // This includes overlapping glyphs on both ends.
+        let line_range = |line| {
+            // TODO: what if not in there (empty line)?
+            let glyphs = self
+                .view
+                .lines
+                .iter()
+                .find(|(index, _)| *index == line)
+                .map(|(_, line)| line.glyphs())
+                .unwrap_or_default();
+
+            // NOTE: we could try to guess which end to start from!
+            // TODO: buggy at line boundaries?
+            let start = || {
+                glyphs
+                    .iter()
+                    .find(|glyph| glyph.range.end as usize > selection.start.column)
+                    .map(|glyph| glyph.offset)
+                    .unwrap_or(
+                        glyphs
+                            .last()
+                            .map(|glyph| glyph.offset + glyph.advance)
+                            .unwrap_or_default(),
+                    ) as u32
+            };
+            let end = || {
+                glyphs
+                    .iter()
+                    .find(|glyph| glyph.range.end as usize >= selection.end.column)
+                    .map(|glyph| glyph.offset + glyph.advance)
+                    .unwrap_or(
+                        glyphs
+                            .last()
+                            .map(|glyph| glyph.offset + glyph.advance)
+                            .unwrap_or_default(),
+                    ) as u32
+            };
+
+            if line == selection.start.line {
+                if line == selection.end.line {
+                    // Single line
+                    (start(), end())
+                } else {
+                    // First line
+                    (start(), draw_width)
+                }
+            } else if line == selection.end.line {
+                // Last line
+                (0, if selection.end.column == 0 { 0 } else { end() })
+            } else {
+                // Middle line
+                debug_assert!(false); // We don't do middle lines
+                (0, draw_width)
+            }
+        };
+
+        let start_line = selection.start.line;
+        let end_line = selection.end.line;
+        let full = self.view.line_height;
+        let half = self.view.line_height / 2;
+        let quarter = self.view.line_height / 4;
+
+        // Caret
+        if selection.start.index == selection.end.index {
+            return;
+        }
+
+        // Single line
+        if start_line == end_line {
+            let top = line_top(start_line);
+            let (start, end) = line_range(start_line);
+            let width = end - start;
+            let radius = quarter.min(width / 2);
+            let left = start as i32;
+
+            // surface.stroke_rect(top, left, width, full, radius, COLOR);
+            self.draw
+                .rectangle([top, left], [width, full], 1, radius, COLOR);
+
+            return;
+        }
+
+        // Two non-overlapping lines
+        if selection.start.line + 1 == selection.end.line
+            && selection.start.column > selection.end.column
+        {
+            let top = line_top(start_line);
+            let bottom = top + full as i32;
+            let (start, end) = line_range(start_line);
+            let width = end - start;
+            let radius = quarter.min(width / 2);
+            let left = start as i32;
+
+            // surface.stroke_corner(TopLeft, top, left, width, half, radius, COLOR);
+            // surface.stroke_corner(BottomLeft, bottom, left, width, half, radius, COLOR);
+
+            let top = line_top(end_line);
+            let bottom = top + full as i32;
+            let (start, end) = line_range(end_line);
+            let width = end - start;
+            let radius = quarter.min(width / 2);
+            let right = end as i32;
+
+            // surface.stroke_corner(TopRight, top, right, width, half, radius, COLOR);
+            // surface.stroke_corner(BottomRight, bottom, right, width, half, radius, COLOR);
+
+            return;
+        }
+
+        // Two (overlapping) lines or more
+        let top1 = line_top(start_line);
+        let bottom1 = top1 + full as i32;
+        let (start1, end1) = line_range(start_line);
+        let left1 = start1 as i32;
+        debug_assert!(end1 == draw_width);
+
+        let top2 = line_top(end_line);
+        let bottom2 = top2 + full as i32;
+        let (start2, end2) = line_range(end_line);
+        let right2 = end2 as i32;
+        debug_assert!(start2 == 0);
+
+        let width = draw_width - start1;
+        let radius = quarter.min(width / 2);
+        // surface.stroke_corner(TopLeft, top1, left1, width, half, radius, COLOR);
+
+        let width = draw_width - end2;
+        let radius = quarter.min(width / 2);
+        // surface.stroke_corner(TopLeft, top2, right2, width, half, radius, COLOR);
+
+        let width = start1;
+        let radius = quarter.min(width / 2);
+        // surface.stroke_corner(BottomRight, bottom1, left1, width, half, radius, COLOR);
+
+        let width = end2;
+        let radius = quarter.min(width / 2);
+        // surface.stroke_corner(BottomRight, bottom2, right2, width, half, radius, COLOR);
     }
 }
